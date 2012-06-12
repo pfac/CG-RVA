b20fae7bfe4ba870536ef6f276581d20dd1e4245
Miguel Palhas <mpalhas@gmail.com>
2012-06-12 03:23:40 +0100

    report fixes

--- naps/js/main.js
+++ naps/js/main.js
@@ -10,9 +10,9 @@ function include(filename) {
 }
 
 include('js/jquery-1.7.2.min.js');
-include('js/jcanvas.min.js');
 include('js/camera.js');
 include('js/params.js');
+include('js/Three.js');
 
 window.onload = onLoad;
 
@@ -20,7 +20,7 @@ window.onload = onLoad;
  * GLOBAL VARS
  *******************/
 
- function MyCanvas(id) {
+function MyCanvas(id) {
  	this.id = id;
  	this.jcanvas  = $(id);
  	this.canvas   = this.jcanvas[0]
@@ -32,7 +32,7 @@ window.onload = onLoad;
  	}
 
  	this.setSize(w, h);
- }
+}
 
 var _camera, _marker, _render_initial, _render_final
 var video;
@@ -41,17 +41,29 @@ var render_initial_canvas;
 var render_final_canvas;
 var marker_ctx;
 
-var param;
-var raster;
-var detector;
-var resultMat;
-
 var ctx;
 var img;
+
+var arMatTemp;	// array to temporarily hold matrix data
+var arParam;
+var arRaster;
+var arDetector;
+var arMarkers;
+
+var renderer; 	// Three Renderer
+var texVideo;	// texture hoding video images
+
+var camAR;		// WebGL camera according to the webcam
+var sceneVideo;
+var sceneAR;	// 3D scene with augmented reality
 
 /********************
  * FUNCS
  *******************/
+
+/**
+ * Initialization of stuff
+ */
 function init() {
 	// Initialize video feed
 	video = $('#camera_video')[0];
@@ -65,15 +77,51 @@ function init() {
 	_render_final   = new MyCanvas("#render_final");
 
 	// JSARToolKit stuff
-	flar_param     = new FLARParam(w, h)
-	detector       = new FLARMultiIdMarkerDetector(flar_param, 80)
-	detector.setContinueMode(true)
-	resultMat = new NyARTransMatResult()
+	arParam     = new FLARParam(w, h);
+	arDetector       = new FLARMultiIdMarkerDetector(arParam, 80);
+	arDetector.setContinueMode(true);
+	resultMat = new NyARTransMatResult();
 
 	// Raster creation for marker detection from the camera canvas
-	raster    = new NyARRgbRaster_Canvas2D(_camera.canvas)
+	arRaster    = new NyARRgbRaster_Canvas2D(_camera.canvas);
+
+	arMatTemp = new Float32Array(16);
+	arParam.copyCameraMatrix(arMatTemp, 1, 2000);
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(w, h);
+	renderer.setClearColorHex(0x777777, 1.0);
+
+	texVideo = new THREE.Texture(_camera.canvas);
+	var bbGeo = new THREE.PlaneGeometry(2, 2, 0);
+	var bbMat = new THREE.MeshBasicMaterial({map: texVideo});
+	var bbPlane = new THREE.Mesh(bbGeo, bbMat);
+	bbPlane.rotation.x += Math.PI / 2;
+	bbPlane.depthTest  = false;
+	bbPlane.depthWrite = false;
+
+	camVideo = new THREE.Camera();
+	sceneVideo = new THREE.Scene();
+
+	sceneVideo.add(bbPlane);
+	sceneVideo.add(camVideo);
+
+	var lightPoint1 = new THREE.PointLight(0xffffff, 2.0);
+	lightPoint1.position.set(400, 500, 100);
+
+	camAR = new THREE.Camera();
+	camAR.projectionMatrix.setFromArray(arMatTemp);
+
+	sceneAR = new THREE.Scene();
+	sceneAR.add(lightPoint1);
+	sceneAR.add(camAR);
+
+	arMarkers = {};
 }
 
+/**
+ * something like glutResizeFunc
+ */
 function updateDimesions() {
 	w = toInt($("#dim_width").text());
 	h = toInt($("#dim_height").text());
@@ -84,53 +132,116 @@ function updateDimesions() {
 	_render_final.setSize(w, h);
 }
 
+/**
+ * image loading
+ */
 function loadImg() {
 	img = new Image()
 	img.src = 'markers/0'// + toInt(Math.random() * 100).toString()
 }
 /////////////////////////////
+/**
+ * something like glutRenderFunc
+ */
 function render() {
 	//canvas.drawRect({x: 0, y: 0});
 
-	var dt = new Date().getTime();
 	_camera.ctx.drawImage(video, 0, 0, w, h);
+	_marker.ctx.drawImage(video, 0, 0, w, h);
 	_camera.canvas.changed = true;
 
-	_marker.ctx.fillStyle = "white";
-	_marker.ctx.fillRect(0, 0, w, h);
+	var ctx = _marker.ctx;
+	//ctx.fillStyle = "white";
+	//ctx.fillRect(0, 0, w, h);
 	//_marker.ctx.drawImage(video, 0, 0, w, h);
 
-	var detect_count = detector.detectMarkerLite(raster, 160);
-
-	var ctx = _marker.ctx;
-	ctx.save();
-		ctx.translate(w/2, h/2);
-		ctx.strokeStyle = 'red';
-		var sc;
-		for(var idx = 0; idx < detect_count; ++idx) {
-			var id = detector.getIdMarkerData(idx);
-			var square = detector.getSquare(idx);
-
-			detector.getTransformMatrix(idx, resultMat);
-
-		ctx = _marker.ctx;
-		sc = 940 / resultMat.m23;
-		ctx.save();
-			ctx.scale(sc, sc);
-			ctx.transform(resultMat.m00, resultMat.m01, -resultMat.m10, -resultMat.m11, resultMat.m03*3/4, resultMat.m13*3/4);
-			ctx.strokeRect(-26, -26, 53, 53);
-		ctx.restore();
+	var detect_count = arDetector.detectMarkerLite(arRaster, 100);
+	
+	for(var i = 0; i < detected; ++i) {
+		var id = arDetector.getIdMarkerData(i);
+		var current;
+		if (id.packetLength > 4)
+			current = -1
+		else {
+			current = 0;
+			for(var j = 0; j < id.packetLength; ++j)
+				current = (current << 8) | id.getPacketData(j);
 		}
-	ctx.restore();
+
+		if (!arMarkers[current])
+			arMarkers[current] = {};
+		
+		arDetector.getTransformMatrix(i, arMatResult);
+
+		arMarkers[current].age = 0;
+		arMarkers[current].transform = Object.asCopy(arMatResult);
+	}
+
+
+	for(var i in arMarkers) {
+		var marker = arMarkers[i];
+		if (!marker.model) {
+			var cubeGeo = new THREE.CubeGeometry(100, 100, 100);
+			var cubeMat = new THREE.MeshLambertMaterial({color: 0xff0000});
+			var cube = new THREE.Mesh(cubeGeo, cubeMat);
+			cube.position.z = -50;
+			cube.doubleSided = true;
+
+			marker.model = new THREE.Object3D();
+			marker.model.matrixAutoUpdate = false;
+			marker.model.add(cube);
+
+			sceneAR.add(marker.model);
+		}
+
+		copyMatrix(marker.transform, arMatTemp);
+		marker.model.matrix.setFromArray(arMatTemp);
+		marker.model.matrixWorldNeedsUpdate = true;
+	}
+
+	renderer.autoClear = false;
+	renderer.clear();
+	renderer.render(sceneVideo, _camera);
+	renderer.clear(0x0, 0xffffffff, 0x0);
+	renderer.render(sceneAR, camAR);
+}
+
+
+
+function copyMatrix(mat, cm) {
+	cm[0] = mat.m00;
+	cm[1] = -mat.m10;
+	cm[2] = mat.m20;
+	cm[3] = 0;
+	cm[4] = mat.m01;
+	cm[5] = -mat.m11;
+	cm[6] = mat.m21;
+	cm[7] = 0;
+	cm[8] = -mat.m02;
+	cm[9] = mat.m12;
+	cm[10] = -mat.m22;
+	cm[11] = 0;
+	cm[12] = mat.m03;
+	cm[13] = -mat.m13;
+	cm[14] = mat.m23;
+	cm[15] = 1;
 }
 
 /*********************
  * EVENTS
  ********************/
 function onLoad() {
-	init()
-	loadImg()
+	init();
+	loadImg();
 
-	setInterval(render, 16)
+	THREE.Matrix4.prototype.setFromArray = function(m) {
+	return this.set(
+		m[0], m[4], m[8], m[12],
+		m[1], m[5], m[9], m[13],
+		m[2], m[6], m[10], m[14],
+		m[3], m[7], m[11], m[15]);
+	}
+
+	setInterval(render, 16);	
 }
 
